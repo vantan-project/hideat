e24ba749d3148a3f82e83b9b8d4359b7f9f47447
tah5882 <me@tah5882.dev>
2025-08-04 16:36:15 +0900

    feat: カテゴリ一覧を取得するAPIエンドポイントを実装

--- back/app/Http/Controllers/CategoryController.php
+++ back/app/Http/Controllers/CategoryController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function index() {
+        $categories = Category::all() -> map(function($category) {
+            return [
+                "id" => $category["id"],
+                "name" => $category["name"]
+            ];
+        });
+        // dd($categories -> toArray());
+
+        return response() -> json(
+            $categories
+        );
+    }
 }
