9ca9378ca1ee49c7f330813ae6c996e8ef4e7027
tah5882 <me@tah5882.dev>
2025-08-06 13:47:11 +0900

    feat: HistoryControllerにstoreおよびindexメソッドを追加

--- back/app/Http/Controllers/HistoryController.php
+++ back/app/Http/Controllers/HistoryController.php
@@ -2,9 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\History;
 use Illuminate\Http\Request;
+use App\Http\Requests\HistoryStoreRequest;
+use App\Http\Requests\HistoryIndexRequest;
 
 class HistoryController extends Controller
 {
-    //
+    public function store(HistoryStoreRequest $request)
+    {
+        $data = $request->validated();
+
+        $history = History::create([
+            'is_google' => $data['isGoogle'],
+            'url' => $data['url'],
+            'location_id' => $data['locationId'],
+        ]);
+
+        return response()->json([
+            'id' => $history->id,
+            'success' => true,
+        ], 201);
+    }
+
+    public function index(HistoryIndexRequest $request)
+    {
+        $histories = History::whereIn('id', $request->input('ids'))->get()->map(function ($history) {
+            return [
+                'id' => $history->id,
+                'isGoogle' => $history->is_google,
+                'url' => $history->url,
+                'locationId' => $history->location_id,
+            ];
+        });
+
+        return response()->json($histories);
+    }
 }
