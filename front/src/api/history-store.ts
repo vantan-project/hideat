4be354c5b5553fed555a47253fb00b9679d6407d
tah5882 <me@tah5882.dev>
2025-08-07 15:38:41 +0900

    feat: historyStore APIを新規作成し、履歴データの保存機能を実装

---
+++ front/src/api/history-store.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+export type HistoryStoreRequest = Array<{
+  isGoogle: boolean;
+  url: string;
+  locationId: string;
+}>;
+
+export type HistoryStoreResponse = {
+    id: number;
+    success: boolean;
+};
+
+export async function historyStore(
+  req: HistoryStoreRequest
+): Promise<HistoryStoreResponse> {
+  const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/history`;
+
+  return axios
+    .post<HistoryStoreResponse>(apiUrl, req)
+    .then((res) => res.data)
+    .catch((err) => {
+      console.warn(err);
+      return {
+        id: 0,
+        success: false,
+      };
+    });
+}
