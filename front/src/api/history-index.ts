d4520d4c5d02dac072bfd10d1c69d7f1caabc28c
tah <me@tah5882.dev>
2025-08-07 15:50:59 +0900

    feat: keep-store APIを新規作成し、データ保存機能を実装 (#22)
    * feat: history-index APIを新規作成し、履歴データの取得機能を実装
    
    * feat: historyStore APIを新規作成し、履歴データの保存機能を実装
    
    * feat: keep-index APIを新規作成し、保存されたデータの取得機能を実装
    
    * feat: keep-store APIを新規作成し、データ保存機能を実装
    
    * feat: HistoryIndexRequestとKeepIndexRequestの型定義を修正し、配列からオブジェクトに変更
    
    * feat: HistoryStoreRequestとKeepStoreRequestの型定義を修正し、配列からオブジェクトに変更
    
    * feat: HistoryIndexRequestとKeepIndexRequestにnameフィールドを追加
    feat: HistoryStoreRequestとKeepStoreRequestにnameフィールドを追加

---
+++ front/src/api/history-index.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+export type HistoryIndexRequest = {
+  ids: Array<number>;
+};
+
+export type HistoryIndexResponse = Array<{
+  id: number;
+  name: string;
+  isGoogle: boolean;
+  url: string;
+  locationId: string;
+}>;
+
+export async function historyIndex(
+  req: HistoryIndexRequest
+): Promise<HistoryIndexResponse> {
+  const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/history/index`;
+
+  return axios
+    .get<HistoryIndexResponse>(apiUrl, { params: req })
+    .then((res) => res.data)
+    .catch((err) => {
+      console.warn(err);
+      return [];
+    });
+}
