7c9b065a1c3c106ddac7802d9ac49924865fedb0
tah <me@tah5882.dev>
2025-08-07 14:31:02 +0900

    feat: MainLayoutにナビゲーションリンクを追加し、ホーム、キープ、履歴ページへの遷移を実装 (#19)

--- front/src/components/layout/main-layout.tsx
+++ front/src/components/layout/main-layout.tsx
@@ -3,6 +3,8 @@
 import { HeroUIProvider, ToastProvider } from "@heroui/react";
 import { LogoIcon } from "../shared/icons/logo-icon";
 import { usePathname } from "next/navigation";
+import { House, Pin, History} from "lucide-react";
+import Link from "next/link";
 
 export type Props = {
   children: React.ReactNode;
@@ -11,6 +13,9 @@ export type Props = {
 export function MainLayout({ children }: Props) {
   const pathname = usePathname();
   const isAdminPage = pathname.startsWith("/admin");
+  const isHomePage = pathname === "/";
+  const isKeepPage = pathname === "/keep";
+  const isHistoryPage = pathname === "/history";
 
   return (
     <HeroUIProvider>
@@ -24,6 +29,37 @@ export function MainLayout({ children }: Props) {
         </>
       )}
       {children}
+      {!isAdminPage && (
+        <div className="fixed bottom-0 left-0 z-50 w-screen grid grid-cols-3 bg-primary px-10 py-2 rounded-tl-4xl rounded-tr-4xl">
+          <Link
+            href="/"
+            className={`py-2 text-white rounded-full ${isHomePage ? "bg-white" : ""}`}
+          >
+            <div className="flex flex-col items-center justify-center">
+              <House className={`w-5 h-5 ${isHomePage ? "text-primary" : "text-white"}`} />
+              <span className={`text-xs mt-1 ${isHomePage ? "text-primary" : "text-white"}`}>ホーム</span>
+            </div>
+          </Link>
+          <Link
+            href="/keep"
+            className={`py-2 text-white rounded-full ${isKeepPage ? "bg-white" : ""}`}
+          >
+            <div className="flex flex-col items-center justify-center">
+              <Pin className={`w-5 h-5 ${isKeepPage ? "text-primary" : "text-white"}`} />
+              <span className={`text-xs mt-1 ${isKeepPage ? "text-primary" : "text-white"}`}>キープ</span>
+            </div>
+          </Link>
+          <Link
+            href="/history"
+            className={`py-2 text-white rounded-full ${isHistoryPage ? "bg-white" : ""}`}
+          >
+            <div className="flex flex-col items-center justify-center">
+              <History className={`w-5 h-5 ${isHistoryPage ? "text-primary" : "text-white"}`} />
+              <span className={`text-xs mt-1 ${isHistoryPage ? "text-primary" : "text-white"}`}>履歴</span>
+            </div>
+          </Link>
+        </div>
+      )}
     </HeroUIProvider>
   );
 }
