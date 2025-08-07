b8f1f895fbf6d7156ca9b2ec5418036c2315854e
tah5882 <me@tah5882.dev>
2025-08-07 15:38:35 +0900

    feat: history-index APIを新規作成し、履歴データの取得機能を実装

---
+++ front/src/api/history-index.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+export type HistoryIndexRequest = Array<{
+  ids: Array<number>;
+}>;
+
+export type HistoryIndexResponse = Array<{
+  id: number;
+  isGoogle: boolean;
+  url: string;
+  locationId: string;
+}>;
+
+export async function historyIndex(
+  req: HistoryIndexRequest
+): Promise<HistoryIndexResponse> {
+  const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/history/index`;
+
+  return axios
+    .get<HistoryIndexResponse>(apiUrl, { params: req })
+    .then((res) => res.data)
+    .catch((err) => {
+      console.warn(err);
+      return [];
+    });
+}
