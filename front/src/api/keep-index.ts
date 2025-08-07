aafb525f48aeb4d7ad1987323574d6e894f90c10
tah5882 <me@tah5882.dev>
2025-08-07 15:38:44 +0900

    feat: keep-index APIを新規作成し、保存されたデータの取得機能を実装

---
+++ front/src/api/keep-index.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+export type KeepIndexRequest = Array<{
+    ids: Array<number>;
+}>;
+
+export type KeepIndexResponse = Array<{
+    id: number;
+    isGoogle: boolean;
+    url: string;
+    locationId: string;
+}>;
+
+export async function keepIndex(
+  req: KeepIndexRequest
+): Promise<KeepIndexResponse> {
+  const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/keep/index`;
+
+  return axios
+    .get<KeepIndexResponse>(apiUrl, { params: req })
+    .then((res) => res.data)
+    .catch((err) => {
+      console.warn(err);
+      return [];
+    });
+}
