cbb43764a65144ddde46fb9c4162b599f7c86034
tah <me@tah5882.dev>
2025-08-07 15:27:33 +0900

    feat: AdminLayoutにサイドナビバーを追加し、店舗情報とユーザーリストへのリンクを実装 (#21)
    * feat: AdminLayoutにサイドナビバーを追加し、店舗情報とユーザーリストへのリンクを実装
    
    * fix: サイドナビバーの表示条件を修正し、ログインページのチェックを適切に更新
    
    * fix: インポートを修正

--- front/src/components/layout/admin-layout.tsx
+++ front/src/components/layout/admin-layout.tsx
@@ -3,6 +3,9 @@
 import { token } from "@/api/token";
 import { GlobalContext } from "@/hooks/use-global-context";
 import { useEffect, useState } from "react";
+import { LogoIcon } from "../shared/icons/logo-icon";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 export type Props = {
   children: React.ReactNode;
@@ -11,6 +14,9 @@ export type Props = {
 export function AdminLayout({ children }: Props) {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   const [restaurantId, setRestaurantId] = useState<number | null>(null);
+  const pathname = usePathname();
+  const isLoginPage = pathname == "/admin/login";
+  const isSignUpPage = pathname == "/admin/sign-up";
 
   useEffect(() => {
     const tokenApi = async () => {
@@ -34,7 +40,28 @@ export function AdminLayout({ children }: Props) {
         setRestaurantId,
       }}
     >
-      {children}
+
+      {/* サイドナビバー */}
+      {(!isLoginPage && !isSignUpPage) && (
+        <aside className="w-56 bg-primary flex flex-col items-center py-6 fixed top-0 left-0 h-screen z-30">
+          <LogoIcon className="w-40 h-auto text-white mb-9" />
+          <nav className="w-full flex flex-col gap-0 mt-2">
+            <Link href="/admin/restaurant" className="mx-2 border-white border-y">
+              <div className="w-full text-center py-5.5 text-white font-bold text-base bg-transparent hover:opacity-50 transition cursor-pointer">
+                店舗情報
+              </div>
+            </Link>
+            <Link href="/admin/user" className="mx-2 border-white border-b">
+              <div className="w-full text-center py-5.5 text-white font-bold text-base bg-transparent hover:opacity-50 transition cursor-pointer">
+                ユーザーリスト
+              </div>
+            </Link>
+          </nav>
+        </aside>
+      )}
+      <div className={!isLoggedIn && !isSignUpPage ? "" : "pl-56"}>
+        {children}
+      </div>
     </GlobalContext.Provider>
   );
 }
